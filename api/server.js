334251a003245e413313f3e18f667dc78f426836
Michael Greenwald <mgreenwaldm@icloud.com>
2021-08-30 17:37:11 -0500

    working

--- api/server.js
+++ api/server.js
@@ -1,10 +1,56 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-const user = require('./users/model')
+const User = require('./users/model')
+
 const server = express()
+server.use(express.json())
+
+// server.get('/api/users', (req,res) => {
+//
+// })
+//
+// server.get('/api/users', (req,res) => {
+//
+// })
+
+server.delete('/api/users/:id', async (req,res) => {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: 'not found',
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+
+})
+
+server.post('/api/users', (req,res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+        User.insert(user)
+            .then(createdUser => {
+                res.status(201).json(createdUser)
+
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                })
+            })
+    }
+    user.insert(user)
+
+})
 
 server.get('/api/users', (req,res) => {
-    user.find()
+    User.find()
         .then(users => {
             res.json(users)
 
@@ -19,8 +65,13 @@ server.get('/api/users', (req,res) => {
 })
 
 server.get('/api/users/:id', (req,res) => {
-    user.findById(req.params.id)
+    User.findById(req.params.id)
         .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist'
+                })
+            }
             res.json(user)
 
         })
@@ -35,7 +86,7 @@ server.get('/api/users/:id', (req,res) => {
 
 server.use('*',(req,res) => {
     res.status(404).json({
-        message: 'not found'
+        message: 'The user with the specified ID does not exist'
     })
 })
 
