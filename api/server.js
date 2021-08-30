6f6c9cae5dd6227aa9ef5c80e7992d1e3d25ecf8
Michael Greenwald <mgreenwaldm@icloud.com>
2021-08-30 17:53:14 -0500

    done

--- api/server.js
+++ api/server.js
@@ -9,9 +9,35 @@ server.use(express.json())
 //
 // })
 //
-// server.get('/api/users', (req,res) => {
-//
-// })
+server.put('/api/users/:id', async (req,res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist',
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for user'
+                })
+            }else{
+              const updatedUser = await User.update(
+                  req.params.id,
+                  req.body
+              )
+                res.status(200).json(updatedUser)
+            }
+
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating user',
+            err: err.message,
+        })
+    }
+
+})
 
 server.delete('/api/users/:id', async (req,res) => {
     const possibleUser = await User.findById(req.params.id)
